5f1083b6a56283d075f5f5e4fdb91b974a97884e
sin-hyun <78088944+sin-hyun@users.noreply.github.com>
2024-06-10 06:06:49 +0900

    Delete CategoryRepository.java

--- CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
