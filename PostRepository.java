125e508671b5c1b782c555e7aa5867278415437c
sin-hyun <78088944+sin-hyun@users.noreply.github.com>
2024-06-10 06:07:19 +0900

    Delete PostRepository.java

--- PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.board.repository;
-
-import com.example.board.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
